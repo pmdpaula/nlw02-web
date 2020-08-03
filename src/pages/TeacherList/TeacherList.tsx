36e4136e2cfca3bc5154c71cc10b214824478409
Pedro Machado de Paula <pmdpaula@yahoo.com.br>
2020-08-03 20:23:56 -0300

    TeacherList com o quadro o professor

--- src/pages/TeacherList/TeacherList.tsx
+++ src/pages/TeacherList/TeacherList.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
 import './TeacherList.css';
 
 const TeacherList = () => {
@@ -27,6 +29,35 @@ const TeacherList = () => {
         </form>
       </PageHeader>
 
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img src="https://avatars1.githubusercontent.com/u/19227876?s=460&u=be4649d2fd412db8b1c718450032134f5a4c2239&v=4" alt="Pedro de Paula"/>
+            <div>
+              <strong>Pedro Machado de Paula</strong>
+              <span>Química</span>
+            </div>
+
+          </header>
+
+          <p>
+            Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.
+            <br /><br />
+            Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Per aumento de cachacis, eu reclamis. Paisis, filhis, espiritis santis. Cevadis im ampola pa arma uma pindureta.
+          </p>
+
+          <footer>
+            <p>
+              Preço/hora
+              <strong>R$ 80,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp"/>
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   );
 };
