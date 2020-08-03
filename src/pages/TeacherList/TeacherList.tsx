31268349fc68932ebc64d1e466cd5d1a1443280f
Pedro Machado de Paula <pmdpaula@yahoo.com.br>
2020-08-03 19:52:08 -0300

    TeacherList com formulário no header

---
+++ src/pages/TeacherList/TeacherList.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import PageHeader from '../../components/PageHeader/PageHeader';
+
+import './TeacherList.css';
+
+const TeacherList = () => {
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os Proffys disponíves." >
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" name="" id="subject"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week_day">Dia da semana</label>
+            <input type="text" name="" id="week_day"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Hora</label>
+            <input type="text" name="" id="time"/>
+          </div>
+
+        </form>
+      </PageHeader>
+
+    </div>
+  );
+};
+
+export default TeacherList;
